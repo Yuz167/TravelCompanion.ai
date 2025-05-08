3a3e2f1011b8a56cad2447d5daa4d66fd7cdd20e
Yuz167 <yuhaozhang2002@163.com>
2025-05-08 14:43:24 -0700

    added images, middleware, and changed global.css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,12 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 
 const HomePage = () => {
   return (
-    <div>HomePage</div>
+    <div>
+      <h1>Welcome Home</h1>
+      <UserButton />
+    </div>
   )
 }
 
